5942600a7fd712f363bd1ceede2953f878cdb400
zakbvoi <isaacswagmlg@gmail.com>
2025-11-25 20:44:37 -0500

    Create yvalue.cpp

---
+++ Cpp program exercises/yvalue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x;
+    cout << "X: ";
+    cin >> x;
+
+    double y = 3*x*x + 7*x - 15;
+
+    cout << "Y = " << y << endl;
+    return 0;
+}
